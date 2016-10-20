9db9da09e7462d1c0baa639b1f940839e83c90fc
tsvetkovpro <285019@gmail.com>
2016-10-20 17:01:14 +0500

    handling local links.
    At this point, clicking any link in the application triggers a undesired browser refresh.  Demonstrates how to handle local, in-app links without breaking external link functionality. He does this by using the local-links NPM module.

--- src/layout.js
+++ src/layout.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import localLinks from 'local-links';
 
 export default React.createClass({
+
+	onClick (event) {
+		const pathname = localLinks.getLocalPathname(event)
+
+		if (pathname) {
+			event.preventDefault()  // tells navigate internaly
+			app.router.history.navigate(pathname)
+		}
+	},
+
 	render () {
 		return (
-			<div>
+			<div onClick={this.onClick}>
 				<nav className='top-nav top-nav-light cf' role='navigation'>
 					<input id='menu-toggle' className='menu-toggle' type='checkbox'/>
-					<label htmlFor='menu-toggle'>Menu</label>
+					<label htmlFor='menu-toggle'>Меню</label>
 					<ul className='list-unstyled list-inline cf'>
-						<li>HubTags</li>
+						<li>ХабТагс</li>
 						<li><a href='/repos'>Репозиторий</a></li>
 						<li className='pull-right'>
 							<a href='/logout'>Выйти</a>
